fce7f2fc75d1bca5e8d2be795f7e0cdfc920813b
Jasjeev <jasjeev99@gmail.com>
2024-06-07 22:44:27 +0530

    Added Dummy Images Link

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import './Cards.css';
-
+// https://dummyimage.com/340x360/eee/aaa
 const Cards = () => {
   return (
     <div >
-<h1 className='text-center'>OUR SERVICES</h1>
-    <div className="cards-container">
-        
-      {[1, 2, 3, 4].map((card, index) => (
-        <div className="card" key={index} style={{ width: '20rem' }}>
-          <img
-            src={`https://via.placeholder.com/350x200?text=Card+Image+$%7Bcard%7D`}
-            className="card-img-top"
-            alt={`Card ${card}`}
-          />
-          <div className="card-body">
-            <h5 className="card-title">Card title {card}</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the bulk of the card's content.
-            </p>
-            <a href="/" className="btn btn-primary">
-              Go somewhere
-            </a>
+      <h1 className='text-center'>OUR SERVICES</h1>
+      <div className="cards-container">
+
+        {[1, 2, 3, 4].map((card, index) => (
+          <div className="card" key={index} style={{ width: '20rem' }}>
+            <img
+              src={`https://via.placeholder.com/350x200?text=Card+Image+$%7Bcard%7D`} 
+              className="card-img-top"
+              alt={`Card ${card}`}
+            />
+            <div className="card-body">
+              <h5 className="card-title">Card title {card}</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up the bulk of the card's content.
+              </p>
+              <a href="/" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -8,20 +8,15 @@ function Home() {
         <div>
             <div className="d-flex justify-content-between">
                 <div className="container">
-
-
                     <ControlledCarousel />
-
                 </div>
-
-
 
                 <div className='container my-5 '>
                     <h1 className="text-center" >KrishiBot</h1>
                     <p className="text-center text-justify-center " >KrishiBot sees all,knows all</p>
                 </div>
-
             </div>
+            
             <Cards />
         </div>
     )
