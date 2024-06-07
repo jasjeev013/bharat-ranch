cbdb33cdde1ea2c9f9e3a4eaa9306b44ace1a0f0
Jasjeev <jasjeev99@gmail.com>
2024-06-07 19:27:42 +0530

    Completed NavBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import NavBar from './components/NavBar';
 import './App.css';
 
 
+
+
+
 function App() {
+  
   return (
     <div className="App">
-
-      <NavBar/>
+   
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 function NavBar() {
     return (
